5fb3b35f8938c30c12efe1de4d55cfe219b44988
ekmartens <ekmrtns@gmail.com>
2017-11-12 15:30:10 -0800

    finished javascript countdown

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 var d = new Date();
-d.setFullYear(2017, 11, 15);
+d.setMonth(11);
+d.setDate(15);
+d.setHours(0);
+d.setMinutes(0);
+d.setSeconds(0);
+d.setMilliseconds(0);
 document.getElementById("releaseDay").innerHTML = d;
 
-var showDaysToStarWars = document.getElementById('swHeader');
+var showDaysToStarWars = document.getElementById("swHeader");
 var today = new Date();
-var starWars = d;
-
-var daysToStarWars = function(){
-  if today < starWars {
-
-  }
 
 
+var changeText = function() {
+  var currentDay = today.getDate();
+  var swRelease = d.getDate();
+  var numDays = (30 - currentDay) + swRelease;
+  swHeader.innerText = numDays;
 }
+
+changeText();
