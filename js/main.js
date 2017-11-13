f6080917652f88218ce6df47e95a9e4a74479092
ekmartens <ekmrtns@gmail.com>
2017-11-12 21:47:08 -0800

    added countdown clock

--- js/main.js
+++ js/main.js
@@ -10,15 +10,28 @@ d.setSeconds(0);
 d.setMilliseconds(0);
 document.getElementById("releaseDay").innerHTML = d;
 
-var showDaysToStarWars = document.getElementById("swHeader");
-var today = new Date();
+var updateText = function(){
+  showCountdown();
+};
 
+var showCountdown = function(){
+    var showDaysToStarWars = document.getElementById("swHeader");
+    var today = new Date();
+    var currentDay = today.getDate();
+    var swDay = d.getDate();
+    var days = (30 - currentDay) + swDay;
+    var currentHour = today.getHours();
+    var hours = 23 - currentHour;
+    var currentMinute = today.getMinutes();
+    var minutes = 60 - currentMinute;
+    var currentSecond = today.getSeconds();
+    var seconds = 60 - currentSecond;
+    var countDown = days + " Days " + hours + " Hrs " + minutes + " Min " + seconds + " Sec ";
+    showDaysToStarWars.innerText = countDown;
 
-var changeText = function() {
-  var currentDay = today.getDate();
-  var swRelease = d.getDate();
-  var numDays = (30 - currentDay) + swRelease;
-  swHeader.innerText = numDays + " Days";
-}
+};
 
-changeText();
+updateText();
+
+var oneSecond = 1000;
+setInterval(updateText, oneSecond);
